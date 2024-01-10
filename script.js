9cc3dbda5fa5d81bb73dede3ef6a7220afd1594e
Daniella Echevestre <d.echevestre@gmail.com>
2024-01-10 11:03:52 -0800

    Added some basic HTML and JS output

---
+++ script.js
@@ -0,0 +1,15 @@
+/*let myFirstname = 'Bezza';
+let myLastname = 'Zelalem';
+console.log(myFirstname , myLastname);*/
+
+/*let myFirstname = 'Jose';
+let myLastname = 'Rocha';
+console.log(myFirstname , myLastname);*/
+
+/*let myFirstname = 'Noah';
+let myLastname = 'Berhanu';
+console.log(myFirstname , myLastname);*/
+
+/*let myFirstname = 'Daniella';
+let myLastname = 'Echevestre';
+console.log(myFirstname , myLastname);*/
